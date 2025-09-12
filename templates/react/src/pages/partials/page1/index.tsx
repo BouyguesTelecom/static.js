46dce8ac3dbe92bfd0cffebcadb09c74e8d7bfda
Paul Naszalyi <paulnaszalyi@yahoo.fr>
2025-09-12 19:55:48 +0200

    Rename pages

---
+++ templates/react/src/pages/partials/page1/index.tsx
@@ -0,0 +1,41 @@
+import React, {useEffect} from "react";
+import page from "./data.json";
+
+export const getStaticProps = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos/1");
+    const todo = await res.json();
+    return {
+        props: {
+            data: todo,
+        }
+    };
+};
+
+interface DataProps {
+    data: {
+        id: number;
+        title: string;
+        userId: number;
+        completed: boolean;
+    };
+}
+
+const Page1: React.FC<DataProps> = ({data}) => {
+    useEffect(() => {
+        console.log("Page1 loaded!");
+    }, []);
+
+    return (
+        <>
+            <p className="title">Page ID: {data?.id}</p>
+            <p>{`${data.completed}`}</p>
+            <h1>{page.title}</h1>
+            <p>{page.description}</p>
+            <button className="button is-primary" onClick={() => console.log(data)}>
+                Click me!
+            </button>
+        </>
+    );
+};
+
+export default Page1;

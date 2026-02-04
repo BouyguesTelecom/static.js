12de7ee3822df5b96c5e535e7e79f3d76525d459
Paul Naszalyi <paulnaszalyi@yahoo.fr>
2026-02-04 18:34:02 +0100

    add: trilogy stylesheet to partials layout

--- templates/react/src/pages/partials/layout.tsx
+++ templates/react/src/pages/partials/layout.tsx
@@ -3,6 +3,10 @@ import React from "react";
 export const Layout: React.FC<{ children: React.ReactNode }> = ({children}) => {
     return (
         <div data-layout="partials">
+            <link
+                rel="stylesheet"
+                href="https://assets.bouyguestelecom.fr/TRILOGY/trilogy-styles@4/default/trilogy.css"
+            />
             {children}
         </div>
     );
